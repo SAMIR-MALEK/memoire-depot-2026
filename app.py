b5a588e1f1063ab1b19d0d1f552398b7a10d5d1e
SAMIR-MALEK <samir.malek@univ-bba.dz>
2026-01-15 12:52:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ PROF_MEMOS_SHEET_ID = "1OnZi1o-oPMUI_W_Ew-op0a1uOhSj006hw_2jrMD6FSE"
 
 STUDENTS_RANGE = "Feuille 1!A1:K1000"
 MEMOS_RANGE = "Feuille 1!A1:N1000"
-PROF_MEMOS_RANGE = "Feuille 1!A1:K1000"
+PROF_MEMOS_RANGE = "Feuille 1!A1:L1000"
 
 # ---------------- تحميل البيانات ----------------
 @st.cache_data(ttl=300)
